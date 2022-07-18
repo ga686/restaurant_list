eb475b9468dd96bf27634c8eccf3094c35330709
Penny Chang <yahusanchang@gmail.com>
2022-07-18 10:13:03 +0800

    新增網頁

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const restaurantList = require('./restaurant.json')
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+    res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+    const restaurant = restaurantList.results.find(item => req.params.restaurant_id === item.id.toString())
+    res.render('show', { restaurant })
+})
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const restaurants = restaurantList.results.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()))
+    res.render('index', { restaurants: restaurants })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Express is listening on http://localhost:${port}`)
+})
