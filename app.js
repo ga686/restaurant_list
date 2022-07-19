7b9e94f00b5276bf8684b653f1abc31ca94d58bd
Penny Chang <yahusanchang@gmail.com>
2022-07-19 20:54:53 +0800

    修正app.js符合Standard 規範

--- app.js
+++ app.js
@@ -12,27 +12,25 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-    const restaurant = restaurantList.results.find(item => req.params.restaurant_id === item.id.toString())
-    res.render('show', { restaurant })
+  const restaurant = restaurantList.results.find(item => req.params.restaurant_id === item.id.toString())
+  res.render('show', { restaurant })
 })
 
 app.get('/search', (req, res) => {
-    const keyword = req.query.keyword.replace(/\s+/g, '')
-    const restaurants = restaurantList.results.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()))
+  const keyword = req.query.keyword.replace(/\s+/g, '')
+  const restaurants = restaurantList.results.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()))
 
-    if (restaurants.length > 0) {
-        res.render('index', { restaurants })
-        return
-    }
-    res.render('empty_page', { keyword })
+  if (restaurants.length > 0) {
+    res.render('index', { restaurants })
+    return
+  }
+  res.render('empty_page', { keyword })
 })
 
-
-
 app.listen(port, () => {
-    console.log(`Express is listening on http://localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
